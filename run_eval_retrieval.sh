ee7fde32aff24b663eacd5d42536ff5da370e8d7
flatton <tomoki.fujihara@abejainc.com>
2024-09-10 10:13:45 +0900

    update shellscript

--- run_eval_retrieval.sh
+++ run_eval_retrieval.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+log=/app/_$(date '+%Y%m%d-%H%M%S').log
+
+
+exec &> >(awk '{print strftime("[%Y/%m/%d %H:%M:%S] "),$0 } { fflush() } ' >> $log)
+
 for model in "BAAI/bge-m3" "bclavie/JaColBERTv2" "answerdotai/JaColBERTv2.5"; do
     logger -t "docker [$$]" -p local1.info '$model started' -f ./logging.txt
     poetry run python -m jmteb \
         --evaluators "src/jmteb/configs/tasks/jmteb_retrieval.jsonnet" \
         --embedder SentenceBertEmbedder \
         --embedder.model_name_or_path $model \
-        --embedder.batch_size 4 \
-        --embedder.device "mps" \
+        --embedder.batch_size 16 \
+        --embedder.device "cuda:1" \
         --save_dir "output/"$model
 done
 
@@ -17,8 +22,8 @@ for model in "intfloat/multilingual-e5-large" "intfloat/multilingual-e5-base" "p
         --evaluators "src/jmteb/configs/jmteb_retrieval_prompt_en.jsonnet" \
         --embedder SentenceBertEmbedder \
         --embedder.model_name_or_path $model \
-        --embedder.batch_size 4 \
-        --embedder.device "mps" \
+        --embedder.batch_size 16 \
+        --embedder.device "cuda:1" \
         --save_dir "output/"$model
 done
 
@@ -28,7 +33,7 @@ for model in "cl-nagoya/ruri-large" "cl-nagoya/ruri-base" "cl-nagoya/ruri-small"
         --evaluators "src/jmteb/configs/jmteb_retrieval_prompt_en.jsonnet" \
         --embedder SentenceBertEmbedder \
         --embedder.model_name_or_path $model \
-        --embedder.batch_size 4 \
-        --embedder.device "ops" \
+        --embedder.batch_size 16 \
+        --embedder.device "cuda:1" \
         --save_dir "output/"$model
 done
