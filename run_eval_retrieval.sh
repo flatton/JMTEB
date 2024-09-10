f6b744439d16844b9cb529e02cd83ae6585c2dbf
flatton <tomoki.fujihara@abejainc.com>
2024-09-10 09:42:45 +0900

    update shellscript

--- run_eval_retrieval.sh
+++ run_eval_retrieval.sh
@@ -5,7 +5,7 @@ for model in "BAAI/bge-m3" "bclavie/JaColBERTv2" "answerdotai/JaColBERTv2.5"; do
         --evaluators "src/jmteb/configs/tasks/jmteb_retrieval.jsonnet" \
         --embedder SentenceBertEmbedder \
         --embedder.model_name_or_path $model \
-        --embedder.batch_size 1 \
+        --embedder.batch_size 8 \
         --embedder.device "mps" \
         --save_dir "output/"$model
 done
@@ -15,7 +15,7 @@ for model in "intfloat/multilingual-e5-large" "intfloat/multilingual-e5-base" "p
         --evaluators "src/jmteb/configs/jmteb_retrieval_prompt_en.jsonnet" \
         --embedder SentenceBertEmbedder \
         --embedder.model_name_or_path $model \
-        --embedder.batch_size 1 \
+        --embedder.batch_size 8 \
         --embedder.device "mps" \
         --save_dir "output/"$model
 done
@@ -25,7 +25,7 @@ for model in "cl-nagoya/ruri-large" "cl-nagoya/ruri-base" "cl-nagoya/ruri-small"
         --evaluators "src/jmteb/configs/jmteb_retrieval_prompt_en.jsonnet" \
         --embedder SentenceBertEmbedder \
         --embedder.model_name_or_path $model \
-        --embedder.batch_size 1 \
+        --embedder.batch_size 8 \
         --embedder.device "ops" \
         --save_dir "output/"$model
 done
